b00aaae4c92004810d761473457c205604234251
Marshall Mickelson <marshallmick+git@gmail.com>
2013-03-25 20:49:37 -0700

    added status reporting

--- recompose.rb
+++ recompose.rb
@@ -43,24 +43,44 @@ end
 
 def compute_new_xy(x, y, options)
   { 
-    :x => x.map_space(0.0, options[:original_x].to_f, 0, options[:output_x].to_f - options[:original_x].to_f),
-    :y => y.map_space(0.0, options[:original_y].to_f, 0, options[:output_y].to_f - options[:original_y].to_f)
+    :x => x.map_space(0.0, options[:original_x].to_f, 0, options[:output_x].to_f - options[:original_x].to_f).to_i,
+    :y => y.map_space(0.0, options[:original_y].to_f, 0, options[:output_y].to_f - options[:original_y].to_f).to_i
   }
 end
 
+def status(xy, f, seq, length, start, actual)
+  pc = (seq / length) * 100.0
+  s1 = seq + 1
+  elapsed = Time.now - start
+  avg = elapsed / actual
+  remaining = length - seq
+  left = ((remaining * avg) / 60.0).round(2)
+  puts "#{xy} => #{f} \n\t(#{s1.to_i}/#{length.to_i} #{pc.round(2)}% done [#{left} min remaining] #{avg.round(2)} sec per frame)"
+end
+
 files = Dir.glob("#{options[:data]}/#{options[:key]}-*.png")
+seq = 0
+actual = 1
+puts "Processing #{files.length} files"
+start = Time.now
 
 files.each do |f|
   tokens = tokenize f
   newfile = File.join( options[:output], tokenstofile(tokens) )
-
-  of = ChunkyPNG::Image.from_file f
-  nf = ChunkyPNG::Image.new options[:output_x], options[:output_y], ChunkyPNG::Color::TRANSPARENT
-  xy = compute_new_xy tokens[2].to_f, tokens[3].to_f, options
-  puts "#{xy} => #{File.basename(f)}"
-  nf.replace! of, xy[:x], xy[:y]
-  nf.save newfile
+  
+  if !File.exists? newfile
+    of = ChunkyPNG::Image.from_file f
+    nf = ChunkyPNG::Image.new options[:output_x], options[:output_y], ChunkyPNG::Color::TRANSPARENT
+    xy = compute_new_xy tokens[2].to_f, tokens[3].to_f, options
+    status xy, File.basename(f), seq.to_f, files.length.to_f, start, actual
+    nf.replace! of, xy[:x], xy[:y]
+    nf.save newfile
+    actual += 1
+  else
+    puts "skipping #{File.basename(f)}"
+  end
+  seq += 1
 end
 
-puts files.length
+
 
